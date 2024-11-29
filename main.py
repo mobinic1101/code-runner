426c5efd1153e2578a2976f5be63597d1c621f45
mobin <abdulmobinkarimi451@gmail.com>
2024-11-30 02:22:07 +0330

    add Pydantic models to structure and validate the incoming data, complete the  view.

--- main.py
+++ main.py
@@ -1,10 +1,26 @@
-from typing import Annotated
+from typing import Annotated, List, Optional, Tuple, Set
 from fastapi import FastAPI, File, UploadFile, Form
+from pydantic import BaseModel
 import run_code
+
+
+class TestCase(BaseModel):
+    id: int
+    input: Tuple = (None)
+    expected: Optional[List]
+
+
+class Data(BaseModel):
+    allowed_imports: Set[str]
+    test_case: List[TestCase]
+
 
 app = FastAPI()
 
 
 @app.post("/run-code")
-def run(python_file: Annotated[UploadFile, File]):
-    pass
+async def run(python_file: Annotated[UploadFile, File], data: Annotated[Data, Form]):
+    for test_case in data.test_case:
+        res = await run_code.execute_code(python_file, test_case.input, data.allowed_imports)
+        res["id"] = test_case.id
+    return res
