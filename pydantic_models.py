ea4e25ddf2b375803fbc8d420fa8e817af9e1508
mobin <abdulmobinkarimi451@gmail.com>
2024-12-03 06:45:13 +0330

    threw pydantic models from main.py to here

---
+++ pydantic_models.py
@@ -0,0 +1,13 @@
+from pydantic import BaseModel
+from typing import Optional, List, Set, Tuple
+
+
+class TestCase(BaseModel):
+    id: int
+    input: Tuple = (None)
+    expected: Optional[List]
+
+
+class Data(BaseModel):
+    allowed_imports: Set[str]
+    test_case: List[TestCase]
