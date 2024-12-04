1068c3471dcf1aff846946f8afb30a0b9e807fbf
mobin <abdulmobinkarimi451@gmail.com>
2024-12-04 06:55:20 +0330

    update file

--- main.py
+++ main.py
@@ -1,26 +1,11 @@
-from typing import Annotated, List, Optional, Tuple, Set
 from fastapi import FastAPI, File, UploadFile, Form
-from pydantic import BaseModel
-import run_code
-
-
-class TestCase(BaseModel):
-    id: int
-    input: Tuple = (None)
-    expected: Optional[List]
-
-
-class Data(BaseModel):
-    allowed_imports: Set[str]
-    test_case: List[TestCase]
+from typing import Annotated
+from . import run_code, pydantic_models
 
 
 app = FastAPI()
 
 
 @app.post("/run-code")
-async def run(python_file: Annotated[UploadFile, File], data: Annotated[Data, Form]):
-    for test_case in data.test_case:
-        res = await run_code.execute_code(python_file, test_case.input, data.allowed_imports)
-        res["id"] = test_case.id
-    return res
+async def run(python_file: Annotated[UploadFile, File], data: Annotated[pydantic_models.Data, Form]):
+    pass
